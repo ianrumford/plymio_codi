0b2a788fd1226ee4ccc59bc4ce7f274c34fba36e
Ian Rumford <ian@taniwha>
2018-02-12 11:03:48 +0000

    plymio_codi: Building Forms for Common Code Patterns

---
+++ lib/plymio_codi.ex
@@ -0,0 +1,18 @@
+defmodule PlymioCodi do
+  @moduledoc """
+  Documentation for PlymioCodi.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PlymioCodi.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/plymio_codi_test.exs
@@ -0,0 +1,8 @@
+defmodule PlymioCodiTest do
+  use ExUnit.Case
+  doctest PlymioCodi
+
+  test "greets the world" do
+    assert PlymioCodi.hello() == :world
+  end
+end
